b1e69c34efad1b22f640b1f161ffec75ded45e6e
David Easton <d.easton@wustl.edu>
2020-05-16 16:52:22 -0400

    annotate roll function

--- src/utils/roll.js
+++ src/utils/roll.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+/*
+    Helper function to simulate a die roll
+    Because every nation has a die with a different number of sides,
+    the die size must be passed as an argument.
+*/
+
 function roll(numSides) {
     return Math.ceil(Math.random() * numSides);
 }
