cc91f43602f4d2b06e76dd39a5fcc84a2ccad18f
David Easton <d.easton@wustl.edu>
2020-05-16 16:38:28 -0400

    start building out nation class

---
+++ src/components/nation/nation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './nation.css';
+
+class nation extends Component {
+    constructor(prop) {
+        super(props);
+    }
+
+    componenetDidMount(){
+        
+    }
+
+}
