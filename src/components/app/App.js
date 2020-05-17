e4e9c31014ef84f79a598731c85e6558900338f8
David Easton <d.easton@wustl.edu>
2020-05-17 12:02:33 -0400

    typos fixed to restore build

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,28 +2,30 @@ import React, {Component} from 'react';
 import logo from '../../assets/images/logo.svg';
 import './app.css';
 
-class App extends Componenet {
+class App extends Component {
   constructor(props){
     super(props);
     // state, with redux probably
   }
   render() {
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
   };
 }
 
