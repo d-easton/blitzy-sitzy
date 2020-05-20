e502e78ad8035b5628069b4f33e564bc6a43bc74
David Easton <d.easton@wustl.edu>
2020-05-20 16:15:09 -0400

    build out first prototype of nation card display

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import * as utils from '../../utils';
 import board from "../board";
 import './app.css';
+import Nation from "../nation/index";
 
 class App extends Component {
   constructor(props){
@@ -11,7 +12,13 @@ class App extends Component {
   render() {
     return(
       <div className="App">
-        <h1>BlitzySitzy Game</h1>
+        <div className = "title-box">
+          <h1>BlitzySitzy Game</h1>
+        </div>
+        <Nation id="DE" nationName={"Germany"}>test</Nation>
+        <Nation id="GB" nationName={"Great Britain"}>test2</Nation>
+        <Nation id="RUS" nationName={"Russia"}>test3</Nation>
+        <Nation id="FR" nationName={"France"}>test4</Nation>
       </div>
     );
   };
--- src/components/nation/nation.js
+++ src/components/nation/nation.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './nation.css';
 
 class Nation extends Component {
-    constructor(props, id) {
-        super(props);
-        this.isActive = true;
-        this.id = id;    // unique # or letter symbol refering to each of the four nations
-        // this.dieSize                // size of die -- should this be a parameter or determined by id?
-        this.location = Nation.startingSpace;
-    }
+    // constructor(props, id) {
+    //     super(props);
+    //     this.isActive = true;
+    //     this.id = id;    // unique # or letter symbol refering to each of the four nations
+    //     // this.dieSize                // size of die -- should this be a parameter or determined by id?
+    //     this.location = Nation.startingSpace;
+    // }
 
     componenetDidMount(){
         
     }
 
+    render() {
+        return(
+            <div id = {this.props.id}>
+                <h1>
+                    {this.props.nationName}
+                </h1>
+            </div>
+        );
+    };
 };
 
 Nation.startingSpace = 0;
