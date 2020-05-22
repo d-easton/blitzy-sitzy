0a1b94f61d9f9495b561b00b8051229d6cddc42d
David Easton <d.easton@wustl.edu>
2020-05-22 17:22:38 -0400

    move pawns action added

---
+++ src/state/actions/movePawns.js
@@ -0,0 +1,12 @@
+/*
+    Redux action function to change position of one or more pawns
+*/
+import {MOVE_PAWNS} from "../constants/action-catalog";
+
+const movePawns = (payload) => {
+    return {
+        type: MOVE_PAWNS, payload
+    }
+}
+
+export default activateNation;
--- src/state/constants/action-catalog.js
+++ src/state/constants/action-catalog.js
@@ -4,3 +4,4 @@
 
 export const ACTIVATE_NATION = "ACTIVATE_NATION";
 export const DEACTIVATE_NATION = "DEACTIVATE_NATION";
+export const MOVE_PAWNS = "MOVE_PAWNS";
