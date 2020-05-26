a89bcb4f9e48bddee036551f63326a8f5c4dcaef
David <d.easton@wustl.edu>
2020-05-26 15:47:05 -0400

    draw button prototype

---
+++ src/components/nation/components/draw-button/draw-button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './draw-button.css';
+
+const DrawButton = (props) => {
+    return <button class={props.nation}> D </button>;   // todo: check prop pass name
+}
+
+export default Nation;
